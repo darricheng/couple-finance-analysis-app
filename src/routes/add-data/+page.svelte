ea2841f3fa74a6da5517c72a1b1435371ade28ca
Darric Heng <darricheng@gmail.com>
2023-03-19 22:45:59 +0800

    Add return home button

--- src/routes/add-data/+page.svelte
+++ src/routes/add-data/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-    let data = "";
-    function addData() {
-        // TODO: function should store the data somewhere for use later
-    }
+	let data = '';
+	function addData() {
+		// TODO: function should store the data somewhere for use later
+	}
 </script>
+
 <h1>Paste your csv data into the text box</h1>
 <form on:submit={addData}>
-    <textarea bind:value={data} />
-    <button type="submit">Submit</button>
+	<textarea bind:value={data} />
+	<button type="submit">Submit</button>
 </form>
 
 <!-- https://stackoverflow.com/a/30593806 -->
 <p style="white-space: pre-line;">{data}</p>
+
+<a href="/">Back home</a>
