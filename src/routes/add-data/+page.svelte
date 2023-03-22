6b892d48b4feb4030b98d016a52c7daf8fde96f0
Darric Heng <darricheng@gmail.com>
2023-03-22 21:36:37 +0800

    Process csv headers

--- src/routes/add-data/+page.svelte
+++ src/routes/add-data/+page.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
 	let name = '';
 	let data = '';
-	function addData() {
+	let lines = [];
+	let headers: String[] = [];
+	function processData() {
 		// TODO: function should store the data somewhere for use later
+		// Take the first row of data and display it for mapping to the required data columns
+		lines = data.split(/\r?\n/);
+		headers = lines[0].split(',');
 	}
 </script>
 
 <h1>Add Data</h1>
-<form on:submit={addData}>
-	<input placeholder="Name" bind:value={name} />
+<form on:submit={processData}>
+	<input placeholder="Enter Your Name" bind:value={name} />
+	<p>Paste your data in csv format with headers</p>
 	<textarea bind:value={data} />
 	<button type="submit">Submit</button>
 </form>
 
 <!-- https://stackoverflow.com/a/30593806 -->
-<p style="white-space: pre-line;">{data}</p>
+<!-- <p style="white-space: pre-line;">{data}</p> -->
+{#if headers.length !== 0}
+	<h2>Map your columns to the required data columns</h2>
+	<ul>
+		{#each headers as header}
+			<li>{header}</li>
+		{/each}
+	</ul>
+{/if}
 
 <a href="/">Back home</a>
