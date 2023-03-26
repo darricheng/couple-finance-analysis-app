e29895f1a6e04a236687e457c78aab356a12afee
Darric Heng <darricheng@gmail.com>
2023-03-26 15:35:31 +0800

    Remove comments and console log

--- src/routes/add-data/+page.svelte
+++ src/routes/add-data/+page.svelte
@@ -25,9 +25,7 @@
 			return (validData = false);
 		}
 		validData = true;
-		// TODO: process the input data and attach it to the user's name using Rust
 		let validCsv = mappedHeaders.join(',') + data.slice(endOfHeaders);
-		console.log(validCsv);
 		invoke('parse_csv_to_state', { csvData: validCsv, name });
 	}
 </script>
