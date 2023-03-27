29b2d54886fa826fc7e83880df4fff9a6b8e54ea
Darric Heng <darricheng@gmail.com>
2023-03-27 22:05:00 +0800

    Add csv_parser test

--- src-tauri/src/csv_parser.rs
+++ src-tauri/src/csv_parser.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use csv;
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct FinanceRecord {
     date: String,
     category: String,
@@ -22,8 +22,8 @@ impl UserRecords {
     }
 }
 
-fn test(foo: String) -> Result<Vec<FinanceRecord>, Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(foo.as_bytes());
+fn vectorize_csv(csv_str: String) -> Result<Vec<FinanceRecord>, Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(csv_str.as_bytes());
     let mut finance_records = Vec::new();
     for result in rdr.deserialize() {
         let record: FinanceRecord = result?;
@@ -35,8 +35,33 @@ fn test(foo: String) -> Result<Vec<FinanceRecord>, Box<dyn Error>> {
 
 #[tauri::command]
 pub fn parse_csv_to_state(name: String, csv_data: String, state: tauri::State<super::State>) {
-    let finance_records = test(csv_data).unwrap();
+    let finance_records = vectorize_csv(csv_data).unwrap();
     let user = UserRecords::new(name, finance_records);
     state.0.lock().unwrap().push(user);
     // todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn csv_to_vec() {
+        let csv_str = "date,,category,amount,
+2023-3-28,some text,Food,-1,Lunch
+2022-9-12,,Income,2,";
+        let vectorized_csv = vec![
+            FinanceRecord {
+                date: "2023-3-28".to_string(),
+                category: "Food".to_string(),
+                amount: -1.0,
+            },
+            FinanceRecord {
+                date: "2022-9-12".to_string(),
+                category: "Income".to_string(),
+                amount: 2.0,
+            },
+        ];
+        assert_eq!(vectorize_csv(csv_str.to_string()).unwrap(), vectorized_csv);
+    }
 }
