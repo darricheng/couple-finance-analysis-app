7868e6352a2672f84c8f261ad328446e8dba7315
Darric Heng <darricheng@gmail.com>
2023-04-15 20:54:11 +0800

    Code structure for formatting the date

--- src-tauri/src/csv_parser.rs
+++ src-tauri/src/csv_parser.rs
@@ -1,10 +1,18 @@
 use std::error::Error;
 
+use chrono::NaiveDate;
 use csv;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct FinanceRecord {
+    date: NaiveDate,
+    category: String,
+    amount: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+struct FinanceRecordCsv {
     date: String,
     category: String,
     amount: f64,
@@ -22,27 +30,39 @@ impl UserRecords {
     }
 }
 
-fn vectorize_csv(csv_str: String) -> Result<Vec<FinanceRecord>, Box<dyn Error>> {
+fn vectorize_csv(csv_str: String) -> Result<Vec<FinanceRecordCsv>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(csv_str.as_bytes());
     let mut finance_records = Vec::new();
     for result in rdr.deserialize() {
-        let record: FinanceRecord = result?;
+        let record: FinanceRecordCsv = result?;
         finance_records.push(record);
     }
 
     Ok(finance_records)
 }
 
+/// Format the provided dates in the csv data into a common NaiveDate format.
+fn format_csv_date(
+    string_date_csv: Vec<FinanceRecordCsv>,
+    date_format: String,
+) -> Vec<FinanceRecord> {
+    todo!()
+}
+
+/// user_date_format will be the string entered by the user indicating the format of their dates.
+/// For example, if the user enters "dd-mm-yyyy", then the string will be parsed so that the
+/// respective data is used to create the NaiveDates for the individual records.
 #[tauri::command]
 pub fn parse_csv_to_state(
     name: String,
     csv_data: String,
+    user_date_format: String,
     state: tauri::State<super::State>,
 ) -> Result<Vec<String>, String> {
-    // TODO: Need to parse data into something more workable, such as chrono?
-    // To also receive date format from user, then format using chrono::NaiveDate::from_ymd_opt and
-    // store accordingly.
-    let finance_records = vectorize_csv(csv_data).map_err(|e| e.to_string())?;
+    let finance_records_string_date = vectorize_csv(csv_data).map_err(|e| e.to_string())?;
+
+    let finance_records = format_csv_date(finance_records_string_date, user_date_format);
+
     let data = UserRecords::new(name, finance_records);
     let mut users_vec = state.0.lock().unwrap();
     users_vec.push(data);
@@ -64,12 +84,12 @@ mod tests {
 2023-3-28,some text,Food,-1,Lunch
 2022-9-12,,Income,2,";
         let vectorized_csv = vec![
-            FinanceRecord {
+            FinanceRecordCsv {
                 date: "2023-3-28".to_string(),
                 category: "Food".to_string(),
                 amount: -1.0,
             },
-            FinanceRecord {
+            FinanceRecordCsv {
                 date: "2022-9-12".to_string(),
                 category: "Income".to_string(),
                 amount: 2.0,
