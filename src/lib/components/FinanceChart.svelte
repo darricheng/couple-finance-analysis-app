f0213042a2d82688d7a3726879dfec9c9f1dbcb5
Darric Heng <darricheng@gmail.com>
2023-06-03 14:28:18 +0800

    Create initial setup for chart

---
+++ src/lib/components/FinanceChart.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Bar } from 'svelte-chartjs';
+	import { Chart, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+	export let data;
+
+	Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+</script>
+
+<div>
+	<Bar {data} options={{ responsive: true }} />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri';
+	import FinanceChart from '$lib/components/FinanceChart.svelte';
 
-	let data;
+	let chartData = {
+		labels: [],
+		datasets: [
+			{
+				label: 'Breakdown by Category',
+				data: []
+			}
+		]
+	};
 	async function getData() {
 		try {
-			data = await invoke('get_data_by_categories');
+			let financeData = await invoke('get_data_by_categories');
+			console.log(financeData);
+			chartData.labels = [...financeData.category];
+			chartData.datasets[0].data = [...financeData.total];
+			console.log(chartData);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+	async function showData() {
+		try {
+			let data = await invoke('get_state');
 			console.log(data);
 		} catch (e) {
 			console.error(e);
@@ -15,4 +35,6 @@
 <h1>Finance Analysis app</h1>
 <a href="/add-data">Add Data</a>
 <h2>Data Analysis</h2>
+<button on:click={showData}>Show Data</button>
 <button on:click={getData}>Get Data</button>
+<FinanceChart bind:data={chartData} />
